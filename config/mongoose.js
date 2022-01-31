a8b6648fad64a19c7c9e55cee43c4582690975ad
ar1hant <arihantdaga100@gmail.com>
2022-01-31 13:48:12 +0530

    edited mongo conn

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todo_db');
+// mongoose.connect('mongodb://localhost/todo_db');
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/todo_db');
+}
 
 const db = mongoose.connection;
 
